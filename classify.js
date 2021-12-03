ce1d16eea7d8435eb58452b3fef69d52ea799dc1
Lin Rubo <rubo.lin@qq.com>
2021-12-03 21:13:08 +0800

    取消上一个修改

--- classify.js
+++ classify.js
@@ -3,7 +3,7 @@ const classes = {
         return /ETF(?:\(QDII\))?$/i.test(name) ? '场内' : '场外';
     },
     '收费方式': name => {
-        return name.match(/(?:A|B|C)(?!A|B|C)/)?.[0] ?? '';
+        return name.match(/[ABC]$/)?.[0] ?? '';
     },
     '特殊标签': name => {
         return name.match(/(?:ETF|联接|增强|指数)(?!ETF|联接|增强|指数)/)?.[0] ?? '';
