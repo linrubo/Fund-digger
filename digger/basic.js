f5ad2d4c787a03c40e39550fc79dce296b030fbd
Lin Rubo <rubo.lin@qq.com>
2023-05-01 19:27:06 +0800

    First commit

---
+++ digger/basic.js
@@ -0,0 +1,25 @@
+import request from '../lib/request.js';
+
+const DEFAULT_VALUE = '';
+
+const patterns = {
+    '基金简称': /<title>(\S+)(?=\u0028\d{6}\u0029)/,
+    '基金规模': />规模\D+([\d\.]+)亿/,
+    '成立日期': /成 立 日\D+([\d\-]{10})<\/td>/,
+    '跟踪误差': /跟踪误差\D+([\d\.]+%)<\/td>/,
+    '分红次数': /总计分红<[^>]+>\s*(\d+)<\/a>/,
+};
+
+const getBasicInfo = async function (code) {
+    const url = `https://fund.eastmoney.com/${code}.html?spm=search`;
+    const response = await request(url);
+    const result = {};
+
+    Object.entries(patterns).forEach(([name, pattern]) => {
+        result[name] = response.match(pattern)?.[1] ?? DEFAULT_VALUE;
+    });
+
+    return result;
+};
+
+export default getBasicInfo;
